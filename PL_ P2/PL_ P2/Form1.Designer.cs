df29eb7c0a625bd1bfc0aea5596a2cf1e01043ff
Nikolai Urman <noeinrus@gmail.com>
2019-11-20 19:43:07 +0300

    First commit

---
+++ PL_ P2/PL_ P2/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PL__P2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.readIndificatorsFile = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // readIndificatorsFile
+            // 
+            this.readIndificatorsFile.Location = new System.Drawing.Point(12, 322);
+            this.readIndificatorsFile.Name = "readIndificatorsFile";
+            this.readIndificatorsFile.Size = new System.Drawing.Size(193, 23);
+            this.readIndificatorsFile.TabIndex = 0;
+            this.readIndificatorsFile.Text = "Считать индификаторы";
+            this.readIndificatorsFile.UseVisualStyleBackColor = true;
+            this.readIndificatorsFile.Click += new System.EventHandler(this.readIndificatorsFile_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(211, 322);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(199, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(398, 304);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 352);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.readIndificatorsFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button readIndificatorsFile;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PL_ P2/PL_ P2/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PL__P2
+{
+    public partial class Form1 : Form
+    {
+        private const string indifFile = "Input_indificators.txt";
+        private enum c_state {Start, Indif, IEnd};
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void readIndificatorsFile_Click(object sender, EventArgs e)
+        {
+            AnalyzeIndifFile();
+        }
+
+        private string[] AnalyzeIndifFile() {
+            string res_str = "";
+            string[] indentificators = new string[0];
+            if (indifFile == "")
+                res_str += "Задано пустое имя файла.";
+            else{
+                try
+                {
+                    using (StreamReader sr = new StreamReader(indifFile))
+                    {
+                        string line = sr.ReadToEnd();
+                        c_state CS = c_state.Start;
+                        int i = 0;
+                        string indentificator = "";
+                        do
+                        {
+                            char simb = line[i];
+                            
+                            switch (CS)
+                            {
+                                case c_state.Start: {
+                                    if ((line[i] >= 'A' && line[i] <= 'Z') || (line[i] >= 'a' && line[i] <= 'z') || (line[i] >= '0' && line[i] <= '9'))
+                                    {
+                                        indentificator = "";
+                                        indentificator += line[i];
+                                        CS = c_state.Indif;
+                                    }
+                                    break;                                  
+                                }
+
+                                case c_state.Indif:{
+                                    if ((line[i] >= 'A' && line[i] <= 'Z') || (line[i] >= 'a' && line[i] <= 'z') || (line[i] >= '0' && line[i] <= '9'))
+                                        indentificator += line[i];
+                                    if (line[i] == ' ') {
+                                        Array.Resize(ref indentificators, indentificators.Length + 1);
+                                        indentificators[indentificators.Length - 1] = indentificator;
+                                        CS = c_state.IEnd;
+                                    }
+                                    break;
+                                }
+
+                                case c_state.IEnd: {
+                                    if ((line[i] >= 'A' && line[i] <= 'Z') || (line[i] >= 'a' && line[i] <= 'z') || (line[i] >= '0' && line[i] <= '9'))
+                                    {
+                                        indentificator = "";
+                                        indentificator += line[i];
+                                        CS = c_state.Indif;
+                                    }
+                                    break;                                  
+                                }
+
+                            }
+                            i++;
+                        } while (i < line.Length);
+                        if(indentificator != "") {
+                            Array.Resize(ref indentificators, indentificators.Length + 1);
+                            indentificators[indentificators.Length - 1] = indentificator;
+                        }
+                    }
+                }
+                catch (FileNotFoundException ex)
+                {
+                    res_str += "\r\n" + ex.Message;
+                }
+            }
+            return indentificators;
+        }
+    }
+}
