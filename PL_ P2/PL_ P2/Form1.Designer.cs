a2afb363cb6b10ec039dda9564ab3b602142285e
Nikolai Urman <noeinrus@gmail.com>
2019-11-26 20:02:52 +0300

    Added hash and rehash functions with bugs

--- PL_ P2/PL_ P2/Form1.Designer.cs
+++ PL_ P2/PL_ P2/Form1.Designer.cs
@@ -31,45 +31,56 @@
             this.readIndificatorsFile = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // readIndificatorsFile
             // 
             this.readIndificatorsFile.Location = new System.Drawing.Point(12, 322);
             this.readIndificatorsFile.Name = "readIndificatorsFile";
-            this.readIndificatorsFile.Size = new System.Drawing.Size(193, 23);
+            this.readIndificatorsFile.Size = new System.Drawing.Size(398, 23);
             this.readIndificatorsFile.TabIndex = 0;
             this.readIndificatorsFile.Text = "Считать индификаторы";
             this.readIndificatorsFile.UseVisualStyleBackColor = true;
-            this.readIndificatorsFile.Click += new System.EventHandler(this.readIndificatorsFile_Click);
+            this.readIndificatorsFile.Click += new System.EventHandler(this.ReadIndificatorsFile_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(211, 322);
+            this.button2.Location = new System.Drawing.Point(12, 377);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(199, 23);
+            this.button2.Size = new System.Drawing.Size(398, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Найти идентификатор";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(398, 304);
             this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 351);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(398, 20);
+            this.textBox2.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(422, 352);
+            this.ClientSize = new System.Drawing.Size(422, 405);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.readIndificatorsFile);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ЯП_П2";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,6 +91,7 @@
         private System.Windows.Forms.Button readIndificatorsFile;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- PL_ P2/PL_ P2/Form1.cs
+++ PL_ P2/PL_ P2/Form1.cs
@@ -1,34 +1,109 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace PL__P2
 {
     public partial class Form1 : Form
     {
+        string[] basicMas;
+        List<string> rehashList;
+
         private const string indifFile = "Input_indificators.txt";
-        private enum c_state {Start, Indif, IEnd};
+        private enum C_state {Start, Indif, IEnd};
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void readIndificatorsFile_Click(object sender, EventArgs e)
+        private void ReadIndificatorsFile_Click(object sender, EventArgs e)
         {
             AnalyzeIndifFile();
         }
 
-        private string[] AnalyzeIndifFile() {
+
+        private void ClearLists() {
+            basicMas = null;
+            basicMas = new string[0];
+            rehashList = new List<string>();
+        }
+
+        private int Hash (string inputText) {
+            int sign = 1;
+            int f = 0;
+            for(int i = 0; i < inputText.Length; i++) {
+                f = sign * (byte)inputText[i]; 
+                sign = sign * -1;
+            } //Вычисление функции по значению ключа
+
+            f = f + 255 * 2; //Cовмещение начала области значений функции с начальным адресом хеш - таблицы (a = 1)
+            f = (f * 10000) / (255 * 4); //Совмещение конца области значений функции с конечным адресомхеш - таблицы(a = 10 000)
+            return f;
+        }
+
+        private bool AddToReHash(string Word) {
+            int i = 0;
+            bool inserted = false;
+            bool isError = false;
+            do {
+                int a = (Hash(Word) + i) % 10000;
+                if(rehashList.Count < a || rehashList[a] == ""){
+                    rehashList.Insert(a, Word);
+                    inserted = true;
+                }
+                if(rehashList[a] == Word){
+                    isError = true;
+                }
+                i++;
+            } while (!inserted && !isError);
+            return inserted;
+        }
+
+        public int FindInReHash(string Word)
+        {
+            int i = 0;
+            int index = -1;
+            bool found = false;
+            bool isError = false;
+            do
+            {
+                int a = (Hash(Word) + i) % 10000;
+                if (rehashList[a] == Word)
+                {
+                    index = a;
+                    found = true;
+                }
+                if (rehashList[a] == "")
+                {
+                    isError = true;
+                }
+                i++;
+            } while (!found && !isError);
+            return index;
+        }
+
+        private void AddToBasicMas(string Word) {
+            Array.Resize(ref basicMas, basicMas.Length + 1);
+            basicMas[basicMas.Length - 1] = Word;
+        }
+
+        public int FindInBasicMas(string Word)
+        {
+            int i = -1;
+            do
+            {
+                i++;
+            } while ((i < basicMas.Length) || (basicMas[i] != Word));
+            if (basicMas[i] != Word)
+                i = -1;
+            return i;
+        }
+
+        private void AnalyzeIndifFile() {
             string res_str = "";
-            string[] indentificators = new string[0];
+            ClearLists();
             if (indifFile == "")
                 res_str += "Задано пустое имя файла.";
             else{
@@ -37,42 +112,41 @@ namespace PL__P2
                     using (StreamReader sr = new StreamReader(indifFile))
                     {
                         string line = sr.ReadToEnd();
-                        c_state CS = c_state.Start;
+                        C_state CS = C_state.Start;
                         int i = 0;
-                        string indentificator = "";
+                        string identificator = "";
                         do
                         {
                             char simb = line[i];
                             
                             switch (CS)
                             {
-                                case c_state.Start: {
+                                case C_state.Start: {
                                     if ((line[i] >= 'A' && line[i] <= 'Z') || (line[i] >= 'a' && line[i] <= 'z') || (line[i] >= '0' && line[i] <= '9'))
                                     {
-                                        indentificator = "";
-                                        indentificator += line[i];
-                                        CS = c_state.Indif;
+                                        identificator = "";
+                                        identificator += line[i];
+                                        CS = C_state.Indif;
                                     }
                                     break;                                  
                                 }
 
-                                case c_state.Indif:{
+                                case C_state.Indif:{
                                     if ((line[i] >= 'A' && line[i] <= 'Z') || (line[i] >= 'a' && line[i] <= 'z') || (line[i] >= '0' && line[i] <= '9'))
-                                        indentificator += line[i];
+                                        identificator += line[i];
                                     if (line[i] == ' ') {
-                                        Array.Resize(ref indentificators, indentificators.Length + 1);
-                                        indentificators[indentificators.Length - 1] = indentificator;
-                                        CS = c_state.IEnd;
+                                        AddToBasicMas(identificator);
+                                        CS = C_state.IEnd;
                                     }
                                     break;
                                 }
 
-                                case c_state.IEnd: {
+                                case C_state.IEnd: {
                                     if ((line[i] >= 'A' && line[i] <= 'Z') || (line[i] >= 'a' && line[i] <= 'z') || (line[i] >= '0' && line[i] <= '9'))
                                     {
-                                        indentificator = "";
-                                        indentificator += line[i];
-                                        CS = c_state.Indif;
+                                        identificator = "";
+                                        identificator += line[i];
+                                        CS = C_state.Indif;
                                     }
                                     break;                                  
                                 }
@@ -80,9 +154,9 @@ namespace PL__P2
                             }
                             i++;
                         } while (i < line.Length);
-                        if(indentificator != "") {
-                            Array.Resize(ref indentificators, indentificators.Length + 1);
-                            indentificators[indentificators.Length - 1] = indentificator;
+                        if(identificator != "") {
+                            AddToBasicMas(identificator);
+                            AddToReHash(identificator);
                         }
                     }
                 }
@@ -91,7 +165,15 @@ namespace PL__P2
                     res_str += "\r\n" + ex.Message;
                 }
             }
-            return indentificators;
+        }
+
+        public void FindWord (string Word) {
+            textBox1.Text = "BM: " + FindInBasicMas(Word).ToString() + "; ReHash: " + FindInReHash(Word).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FindWord(textBox2.Text);
         }
     }
 }
